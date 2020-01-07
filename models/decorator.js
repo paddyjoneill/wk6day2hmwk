9c00bf5d5994d29e78945ce454571f88248fb4f3
Patrick O'Neill <paddyjoneill@hotmail.com>
2020-01-07 19:54:14 +0000

    extensions done

--- models/decorator.js
+++ models/decorator.js
@@ -24,9 +24,41 @@ Decorator.prototype.hasEnoughPaint = function(room) {
 Decorator.prototype.paintRoom = function (room) {
   if (this.hasEnoughPaint(room)){
     room.painted = true
+    this.removePaintFromStock(room)
   }
 }
 
+Decorator.prototype.removePaintFromStock = function(room) {
+  let paintNeeded = room.area
+
+  while (paintNeeded > 0) {
+
+    let lastIndex = this.stock.length - 1
+
+    if (paintNeeded < this.stock[lastIndex].amount){
+      this.stock[lastIndex].amount -= paintNeeded
+      paintNeeded = 0
+    }
+    else if (paintNeeded === this.stock[lastIndex].amount) {
+      this.stock.pop()
+      paintNeeded = 0
+    }
+    else if (paintNeeded > this.stock[lastIndex].amount) {
+      paintNeeded -= this.stock[lastIndex].amount
+      this.stock.pop()
+    }
+  }
+}
+
+Decorator.prototype.removeEmptyTins = function () {
+  let i = this.stock.length - 1
+  for (let i = this.stock.length -1 ; i >= 0; i--) {
+    if (this.stock[i].amount === 0) {
+      this.stock.pop()
+    }
+  }
+
+}
 
 
 
